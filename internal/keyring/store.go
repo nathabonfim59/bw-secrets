0e2323b376ebe5a6e21de96719a7249f7f0c1927
nathabonfim59 <dev@nathabonfim59.com>
2026-05-04 06:18:09 +0000

    fix(keyring): add file-based fallback when OS keyring is unavailable

--- internal/keyring/store.go
+++ internal/keyring/store.go
@@ -3,6 +3,8 @@ package keyring
 import (
 	"encoding/json"
 	"errors"
+	"os"
+	"path/filepath"
 
 	"github.com/zalando/go-keyring"
 )
@@ -27,16 +29,23 @@ func Save(creds *Credentials) error {
 	if err != nil {
 		return err
 	}
-	return keyring.Set(serviceName, keyName, string(data))
+	err = keyring.Set(serviceName, keyName, string(data))
+	if err != nil {
+		return fileSave(string(data))
+	}
+	return nil
 }
 
 func Load() (*Credentials, error) {
 	data, err := keyring.Get(serviceName, keyName)
-	if errors.Is(err, keyring.ErrNotFound) {
-		return nil, ErrNotLoggedIn
-	}
 	if err != nil {
-		return nil, err
+		if errors.Is(err, keyring.ErrNotFound) {
+			return nil, ErrNotLoggedIn
+		}
+		data, err = fileLoad()
+		if err != nil {
+			return nil, err
+		}
 	}
 	var creds Credentials
 	if err := json.Unmarshal([]byte(data), &creds); err != nil {
@@ -46,9 +55,49 @@ func Load() (*Credentials, error) {
 }
 
 func Delete() error {
-	err := keyring.Delete(serviceName, keyName)
-	if errors.Is(err, keyring.ErrNotFound) {
-		return nil
+	keyring.Delete(serviceName, keyName)
+	return fileDelete()
+}
+
+func filePath() (string, error) {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
 	}
-	return err
+	return filepath.Join(configDir, "bw-secrets", "credentials.json"), nil
+}
+
+func fileSave(data string) error {
+	path, err := filePath()
+	if err != nil {
+		return err
+	}
+	if err := os.MkdirAll(filepath.Dir(path), 0700); err != nil {
+		return err
+	}
+	return os.WriteFile(path, []byte(data), 0600)
+}
+
+func fileLoad() (string, error) {
+	path, err := filePath()
+	if err != nil {
+		return "", err
+	}
+	data, err := os.ReadFile(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return "", ErrNotLoggedIn
+		}
+		return "", err
+	}
+	return string(data), nil
+}
+
+func fileDelete() error {
+	path, err := filePath()
+	if err != nil {
+		return err
+	}
+	os.Remove(path)
+	return nil
 }
